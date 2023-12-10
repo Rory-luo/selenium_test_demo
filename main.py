dabae23bed3c7cf4e1901eef7d2661cfeba3a44a
Rory-luo <55289804+Rory-luo@users.noreply.github.com>
2023-12-11 03:34:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-import logging
+from selenium.webdriver.chrome.options import Options
+import os
 
-# 初始化Chrome浏览器
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--headless")  # 无头模式，不弹出浏览器窗口
-
-# 使用WebDriverManager来自动下载并获取ChromeDriver路径，指定版本
-chrome_driver_path = ChromeDriverManager(version="94.0.4606.61").install()
-chrome_service = ChromeService(chrome_driver_path)
-browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
-
-# 打开指定网页
-url = "https://blog.csdn.net/CSDN_430422/article/details/130871320?ops_request_misc=&request_id=&biz_id=102&utm_term=%E7%88%AC%E8%99%AB&utm_medium=distribute.pc_search_result.none-task-blog-2~blog~sobaiduweb~default-0-130871320.pc_edu_default&spm=1018.2226.3001.4450"
-browser.get(url)
-
-# 等待页面加载，可以根据实际情况调整等待时间
-browser.implicitly_wait(5)
-
-# 获取页面的所有文字内容
-all_text_elements = browser.find_elements(By.XPATH, "//body//text()")
-
-# 输出文字内容
-for element in all_text_elements:
-    print(element.text)
-
-logging.basicConfig(level=logging.INFO)
-
-# 关闭浏览器
-browser.quit()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chromedriver = "/usr/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chrome_options=chrome_options,executable_path=chromedriver)
+driver.get("https://www.baidu.com")
+print(driver.title)
+driver.quit()
